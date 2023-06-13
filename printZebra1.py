b9e093ca0f35e137ccf75cb3c38e7888020c4a4c
William Novak <william.novak@sft.inf.br>
2023-06-13 15:21:27 -0300

    print zebra text

---
+++ printZebra1.py
@@ -0,0 +1,31 @@
+from zebra import Zebra
+
+# Specify the path to your text file
+file_path = './rotulo zebra/rotulo.txt'
+# './etiqueta_diamante/etiqueta.txt'
+
+# Read the contents of the text file
+# with open(file_path, 'r') as file:
+#     file_contents = file.read()
+
+file_contents  = "^XA"
+file_contents += "^LH10,10"
+file_contents += "^A0N,30^FT10,40^FDLOTE: 11342071^FS"
+file_contents += "^A0N,30^FT300,40^FDOS: 853515 2/4^FS"
+file_contents += "^A0N,30^FT550,40^FDFAB: 10^FS"
+file_contents += "^A0N,30^FT550,430^FDI1342-318^FS"
+file_contents += "^A0N,80^FT680,80^FD34^FS"
+file_contents += "^FT50,150^BY2^BCN,75,Y,N"
+file_contents += "^FD7891234348173^FS"
+file_contents += "^XGE:1342318.GRF^FT525,350^FS"
+file_contents += "^A1N,20^FT10,220^FDCOR 1: MALHA ACTIVE - PRETO^FS"
+file_contents += "^A1N,20^FT10,245^FDELASTICO 1: _ - PRETO^FS"
+file_contents += "^A1N,20^FT10,270^FDELASTICO 2: _ - PRETO^FS"
+file_contents += "^A1N,20^FT10,295^FDFORRO CACHAREL: FORRO CACHAREL - PRETO^FS"
+file_contents += "^A1N,20^FT10,320^FDPALMILHA INTERNA: FORRO CACHAREL - PRETO^FS"
+file_contents += "^A1N,20^FT10,345^FDSOLADO: _ - BRANCO TOFU^FS"
+file_contents += "^PQ1"
+file_contents += "^XZ"
+
+z = Zebra('Zebra GC420t - ZPL')
+z.output(file_contents)
